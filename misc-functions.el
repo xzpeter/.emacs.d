d658242824df667863fd73a24ccd9f8c41c5f589
xzpeter <xzpeter@gmail.com>
2015-08-21 16:38:02 +0800

    Fix tmux buffer clipboard path.

--- misc-functions.el
+++ misc-functions.el
@@ -69,7 +69,7 @@ in ORG MODE. "
 
 (defun my-read-tmux-buffer ()
   (interactive)
-  (evil-read nil "/mac:~/.tmux-buffer"))
+  (evil-read nil "~/.tmux-buffer"))
 
 (defun my-grep-find ()
   "grep and find current word"
